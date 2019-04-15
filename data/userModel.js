0cb62ffe061aeeaad0b0c61a49cb6d0ea3591da2
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-04-15 17:51:05 -0400

    t

--- data/userModel.js
+++ data/userModel.js
@@ -16,8 +16,8 @@ function findBy(filter) {
 }
 
 async function add(user) {
-  const success = 'success'
-  const [id] = await db('users').insert(user);
+  const [id] = await db('users').insert(user).returning(id);
+  return findById(id);
 }
 
 function findById(id) {
